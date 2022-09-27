f031ca3a642b9ba2263901873dd6f37df529e0f2
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-27 16:08:55 -0400

    Update certEnrollPostInstallScript.sh

--- certEnrollPostInstallScript.sh
+++ certEnrollPostInstallScript.sh
@@ -68,22 +68,22 @@ echo "****************************"
 echo "It can take a very long time for the TAK service to come back online after reboot (5min or so)."
 echo " "
 echo "You can check the status of the startup by tailing the logs like so:"
-echo "docker exec -it tak-server_tak_1 /bin/bash"
+echo "docker exec -it tak-server-tak-1 /bin/bash"
 echo "tail -f /opt/tak/logs/takserver.log"
 echo " "
 echo "You want to wait for the 'Waiting for the API process...' to stop and show the plugin service has started before you try to login to the admin web UI. Example below:"
-printf "2022-09-27-19:49:53.476 [main] t.s.plugins.service.PluginService - Waiting for the API process...\n
-2022-09-27-19:49:54.477 [main] t.s.plugins.service.PluginService - Waiting for the API process...\n
-2022-09-27-19:49:55.478 [main] t.s.plugins.service.PluginService - Waiting for the API process...\n
-2022-09-27-19:49:56.482 [main] t.s.plugins.service.PluginService - Waiting for the API process...\n
-2022-09-27-19:49:57.484 [main] t.s.plugins.service.PluginService - Waiting for the API process...\n
-2022-09-27-19:49:58.485 [main] t.s.plugins.service.PluginService - Waiting for the API process...\n
-2022-09-27-19:50:00.128 [main] t.s.plugins.service.PluginService - pluginMissionApi available: true\n
-2022-09-27-19:50:01.209 [main] com.zaxxer.hikari.HikariDataSource - HikariPool-1 - Starting...\n
-2022-09-27-19:50:01.272 [main] com.zaxxer.hikari.HikariDataSource - HikariPool-1 - Start completed.\n
-2022-09-27-19:50:01.306 [main] t.s.ignite.IgniteConfigurationHolder - ignite configuration not populated yet\n
-2022-09-27-19:50:01.425 [main] t.s.p.messaging.PluginMessenger - create PluginMessenger\n
-2022-09-27-19:50:04.669 [main] t.s.p.manager.loader.PluginLoader - starting PluginLoader\n
-2022-09-27-19:50:04.855 [main] t.s.p.messaging.PluginMessenger - starting PluginMessenger\n
-2022-09-27-19:50:04.887 [main] t.s.plugins.service.PluginService - Started PluginService in 166.325 seconds (JVM running for 208.108)\n
+printf "2022-09-27-19:49:53.476 [main] t.s.plugins.service.PluginService - Waiting for the API process...
+2022-09-27-19:49:54.477 [main] t.s.plugins.service.PluginService - Waiting for the API process...
+2022-09-27-19:49:55.478 [main] t.s.plugins.service.PluginService - Waiting for the API process...
+2022-09-27-19:49:56.482 [main] t.s.plugins.service.PluginService - Waiting for the API process...
+2022-09-27-19:49:57.484 [main] t.s.plugins.service.PluginService - Waiting for the API process...
+2022-09-27-19:49:58.485 [main] t.s.plugins.service.PluginService - Waiting for the API process...
+2022-09-27-19:50:00.128 [main] t.s.plugins.service.PluginService - pluginMissionApi available: true
+2022-09-27-19:50:01.209 [main] com.zaxxer.hikari.HikariDataSource - HikariPool-1 - Starting...
+2022-09-27-19:50:01.272 [main] com.zaxxer.hikari.HikariDataSource - HikariPool-1 - Start completed.
+2022-09-27-19:50:01.306 [main] t.s.ignite.IgniteConfigurationHolder - ignite configuration not populated yet
+2022-09-27-19:50:01.425 [main] t.s.p.messaging.PluginMessenger - create PluginMessenger
+2022-09-27-19:50:04.669 [main] t.s.p.manager.loader.PluginLoader - starting PluginLoader
+2022-09-27-19:50:04.855 [main] t.s.p.messaging.PluginMessenger - starting PluginMessenger
+2022-09-27-19:50:04.887 [main] t.s.plugins.service.PluginService - Started PluginService in 166.325 seconds (JVM running for 208.108)
 "
