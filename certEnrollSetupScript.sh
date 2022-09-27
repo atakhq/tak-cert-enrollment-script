e8be5f466df25611681b96ce5e8be0c8f778387a
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-27 09:50:57 -0400

    Update certEnrollSetupScript.sh

--- certEnrollSetupScript.sh
+++ certEnrollSetupScript.sh
@@ -3,53 +3,17 @@
 echo "YOU MUST RUN THIS FROM INSIDE YOUR TAK DOCKER CONTAINER!!!!"
 read -p "Press any key to being setup..."
 
-#install deps
-apt-get install ed
 
 #Make the int cert and edit the tak config to use it
 cd /opt/tak/certs/
 ./makeCert.sh ca intermediate-CA
 
 #Add new conx type
-ed CoreConfig.xml
-4i
-        <input _name="cassl" auth="x509" protocol="tls" port="8089" />
-.
-w
-q
+sed -i '4 a\         <input _name="cassl" auth="x509" protocol="tls" port="8089" />' /opt/tak/CoreConfig.xml
 
 #Remove old CA config
-sed -i -e '58,69d' CoreConfig.xml
+sed -i -e '58,69d' /opt/tak/CoreConfig.xml
 
 #Add new CA Config
-ed /opt/tak/CoreConfig.xml
-58i
-    <certificateSigning CA="TAKServer">
-     <certificateConfig>
-         <nameEntries>
-             <nameEntry name="O" value="TAK"/>
-             <nameEntry name="OU" value="TAK"/>
-         </nameEntries>
-     </certificateConfig>
-     <TAKServerCAConfig
-     keystore="JKS"
-     keystoreFile="/opt/tak/certs/files/intermediate-CA-signing.jks"
-     keystorePass="atakatak"
-     validityDays="30"
-     signatureAlg="SHA256WithRSA"/>
-    </certificateSigning>
-    <security>
-        <tls
-        keystore="JKS"
-        keystoreFile="/opt/tak/certs/files/takserver.jks"
-        keystorePass="atakatak"
-        crlFile="/opt/tak/certs/files/intermediate-CA.crl"
-        truststore="JKS"
-        truststoreFile="/opt/tak/certs/files/truststore-intermediate-CA.jks"
-        truststorePass="atakatak"
-        context="TLSv1.2"
-        keymanager="SunX509"/>
-    </security>
-.
-w
-q
+sed -i '58 a\ <certificateSigning CA="TAKServer">\n     <certificateConfig>\n         <nameEntries>\n             <nameEntry name="O" value="TAK"/>\n             <nameEntry name="OU" value="TAK"/>\n         </nameEntries>\n     </certificateConfig>\n     <TAKServerCAConfig\n     keystore="JKS"\n     keystoreFile="/opt/tak/certs/files/intermediate-CA-signing.jks"\n     keystorePass="atakatak"\n     validityDays="30"\n     signatureAlg="SHA256WithRSA"/>\n    </certificateSigning>\n    <security>\n        <tls\n        keystore="JKS"\n        keystoreFile="/opt/tak/certs/files/takserver.jks"\n        keystorePass="atakatak"\n        crlFile="/opt/tak/certs/files/intermediate-CA.crl"\n        truststore="JKS"\n        truststoreFile="/opt/tak/certs/files/truststore-intermediate-CA.jks"\n        truststorePass="atakatak"\n        context="TLSv1.2"\n        keymanager="SunX509"/>\n    </security>' /opt/tak/CoreConfig.xml
+
