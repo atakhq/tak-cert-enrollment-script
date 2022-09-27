7a5c8e9401906fc7815adf29b78391f241599cdc
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-27 16:36:01 -0400

    Update certEnrollSetupScript.sh

--- certEnrollSetupScript.sh
+++ certEnrollSetupScript.sh
@@ -9,19 +9,19 @@ cd /opt/tak/certs/
 ./makeCert.sh ca intermediate-CA
 
 #Add new conx type
-sed -i '5 a\        <input _name="cassl" auth="x509" protocol="tls" port="8089" />' /opt/tak/CoreConfig.xml
+sed -i '4 a\        <input _name="cassl" auth="x509" protocol="tls" port="8089" />' /opt/tak/CoreConfig.xml
 
 #Remove old CA config
 sed -i '65d' /opt/tak/CoreConfig.xml
 
 #Add new CA Config
-sed -i '65 a\            <TAKServerCAConfig keystore="JKS" keystoreFile="/opt/tak/certs/files/intermediate-CA-signing.jks" keystorePass="atakatak" validityDays="30" signatureAlg="SHA256WithRSA"/>' /opt/tak/CoreConfig.xml
+sed -i '64 a\        <TAKServerCAConfig keystore="JKS" keystoreFile="/opt/tak/certs/files/intermediate-CA-signing.jks" keystorePass="atakatak" validityDays="30" signatureAlg="SHA256WithRSA"/>' /opt/tak/CoreConfig.xml
 
 #Remove old TLS config
 sed -i '68d' /opt/tak/CoreConfig.xml
 
 #Add new TLS Config
-sed -i '68 a\        <tls keystore="JKS" keystoreFile="/opt/tak/certs/files/takserver.jks" keystorePass="atakatak" crlFile="/opt/tak/certs/files/intermediate-CA.crl" truststore="JKS" truststoreFile="/opt/tak/certs/files/truststore-intermediate-CA.jks" truststorePass="atakatak" context="TLSv1.2" keymanager="SunX509"/>' /opt/tak/CoreConfig.xml
+sed -i '67 a\        <tls keystore="JKS" keystoreFile="/opt/tak/certs/files/takserver.jks" keystorePass="atakatak" crlFile="/opt/tak/certs/files/intermediate-CA.crl" truststore="JKS" truststoreFile="/opt/tak/certs/files/truststore-intermediate-CA.jks" truststorePass="atakatak" context="TLSv1.2" keymanager="SunX509"/>' /opt/tak/CoreConfig.xml
 
 
 echo ""
